de392a65897a63b355ede39831e71379aa81233a
Cristina Fernández Fernández <cristina.98fdz@gmail.com>
2024-11-07 19:29:24 +0100

    change

--- Tema4/Ejercicio8.java
+++ Tema4/Ejercicio8.java
@@ -31,7 +31,7 @@ public class Ejercicio8 {
             double discriminante = Math.pow(b, 2) - 4 * a * c;
 
             // Determinar el número de soluciones en función del discriminante
-            if (discriminante >= 0) {
+            if (discriminante > 0) {
                 // Dos soluciones reales
                 double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
                 double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
